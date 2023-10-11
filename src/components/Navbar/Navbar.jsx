fcf7332632c6ac82ce8e00472c2bc50104735444
abdulrehmansafdar <abdulrehmansafdar2928@gmail.com>
2023-10-11 14:00:45 +0500

    navbar section and color theme updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,92 +3,176 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import "./navbar.css";
 import { DataContainer } from "../../App";
 import { Link } from "react-router-dom";
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 const NavBar = () => {
-  const {CartItem,setCartItem} =useContext(DataContainer);
+  const { CartItem, setCartItem } = useContext(DataContainer);
   const [expand, setExpand] = useState(false);
-  const [isFixed, setIsFixed] = useState(false);
+  const [isFixed, setIsFixed] = useState(true);
   // fixed Header
   function scrollHandler() {
     if (window.scrollY >= 100) {
-        setIsFixed(true);
-    } else if(window.scrollY <= 50) {
-        setIsFixed(false);
+      setIsFixed(true);
+    } else if (window.scrollY <= 50) {
+      setIsFixed(false);
     }
   }
   window.addEventListener("scroll", scrollHandler);
-  useEffect(()=> {
-    if(CartItem.length ===0) {
+  useEffect(() => {
+    if (CartItem.length === 0) {
       const storedCart = localStorage.getItem("cartItem");
       setCartItem(JSON.parse(storedCart));
     }
-  },[])
+  },)
   return (
+    <>
       <Navbar
-      fixed="top"
-      expand="md"
-      className={isFixed ? "navbar fixed":"navbar"}
+        fixed="top"
+        expand="md"
+        className={isFixed ? "navbar fixed" : "navbar"}
       >
-        <Container className="navbar-container">
-            <Navbar.Brand to="/">
-              <ion-icon name="bag"></ion-icon>
-              <h1 className="logo">Multimart</h1>
-            </Navbar.Brand>
-            {/* Media cart and toggle */}
-            <div className="d-flex">
-              <div className="media-cart">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="nav-icon">
-                    <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
-                  </svg>
-                  <Link aria-label="Go to Cart Page" to='/cart' className='cart' data-num={CartItem.length}>
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="nav-icon">
-                    <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
-                  </svg>
-                  </Link>
-              </div>
-              <Navbar.Toggle
+        <Container className="navbar-container col-12">
+          <Navbar.Brand to="/" className="col-4 justify-content-start">
+            <ion-icon name="bag"></ion-icon>
+            <h1 className="logo">Gadgit Glamour</h1>
+          </Navbar.Brand>
+
+          <InputGroup className="col-4">
+            <Form.Control
+              placeholder="Search Gadgets"
+              aria-label="Search Gadgets"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-secondary" id="button-addon2">
+              Search Here
+            </Button>
+          </InputGroup>
+          {/* Media cart and toggle */}
+          <div className="d-flex">
+            <div className="media-cart">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="nav-icon">
+                <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+              </svg>
+              <Link aria-label="Go to Cart Page" to='/cart' className='cart' data-num={CartItem.length}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="nav-icon">
+                  <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                </svg>
+              </Link>
+            </div>
+            <Navbar.Toggle
               aria-controls="basic-navbar-nav"
               onClick={() => {
-                  setExpand(expand ? false : "expanded");
+                setExpand(expand ? false : "expanded");
               }}
-              >
+            >
               <span></span>
               <span></span>
               <span></span>
-              </Navbar.Toggle>
-            </div>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Item>
-                  <Link aria-label="Go to Home Page" className="navbar-link" to="/" onClick={() => setExpand(false)}>
-                      <span className="nav-link-label">Home</span>
-                  </Link>
-                </Nav.Item>
-
-                <Nav.Item>
-                <Link aria-label="Go to Shop Page" className="navbar-link" to="/shop" onClick={() => setExpand(false)}>
-                    <span className="nav-link-label">Shop</span>
-                </Link>
-                </Nav.Item>
-
-                <Nav.Item>
-                <Link aria-label="Go to Cart Page" className="navbar-link" to="/cart" onClick={() => setExpand(false)}>
-                    <span className="nav-link-label">Cart</span>
-                </Link>
-                </Nav.Item>
-                <Nav.Item className="expanded-cart">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="nav-icon">
-                    <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
-                  </svg>
-                  <Link aria-label="Go to Cart Page" to='/cart' className='cart' data-num={CartItem.length}>
+            </Navbar.Toggle>
+          </div>
+          <Navbar.Collapse id="basic-navbar-nav col-4">
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Item className="expanded-cart">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="nav-icon">
+                  <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+                </svg>
+                <Link aria-label="Go to Cart Page" to='/cart' className='cart' data-num={CartItem.length}>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="nav-icon">
                     <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
                   </svg>
-                  </Link>
-                </Nav.Item>
-              </Nav>
-        </Navbar.Collapse>
+                </Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
+      <hr className="m-0" />
+      <Navbar
+        fixed="top"
+        expand="sm"
+        className={isFixed ? "navbar fixed" : "navbar"}
+      >
+        <Container className="navbar-container">
+          <DropdownButton id="dropdown-item-button" size="lg" title="Shop By Category">
+            <Dropdown.Item as="button">Headphones</Dropdown.Item>
+            <Dropdown.Item as="button">Data Cables</Dropdown.Item>
+            <Dropdown.Item as="button">Power Banks</Dropdown.Item>
+            <Dropdown.Item as="button">Headphones</Dropdown.Item>
+            <Dropdown.Item as="button">Data Cables</Dropdown.Item>
+            <Dropdown.Item as="button">Power Banks</Dropdown.Item>
+            <Dropdown.Item as="button">Headphones</Dropdown.Item>
+            <Dropdown.Item as="button">Data Cables</Dropdown.Item>
+            <Dropdown.Item as="button">Power Banks</Dropdown.Item>
+          </DropdownButton>
+          {/* Media cart and toggle */}
+          <div className="d-flex">
+            <div className="media-cart">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="nav-icon">
+                <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+              </svg>
+              <Link aria-label="Go to Cart Page" to='/cart' className='cart' data-num={CartItem.length}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="nav-icon">
+                  <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                </svg>
+              </Link>
+            </div>
+            <Navbar.Toggle
+              aria-controls="basic-navbar-nav"
+              onClick={() => {
+                setExpand(expand ? false : "expanded");
+              }}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </Navbar.Toggle>
+          </div>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Item>
+                <Link aria-label="Go to Home Page" className="navbar-link" to="/" onClick={() => setExpand(false)}>
+                  <span className="nav-link-label">Home</span>
+                </Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Link aria-label="Go to Featured products Page" className="navbar-link" to="/featured products" onClick={() => setExpand(false)}>
+                  <span className="nav-link-label">Featured Products</span>
+                </Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Link aria-label="Go to All products Page" className="navbar-link" to="/all products" onClick={() => setExpand(false)}>
+                  <span className="nav-link-label">All Products</span>
+                </Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Link aria-label="Go to Contact Us Page" className="navbar-link" to="/contact us" onClick={() => setExpand(false)}>
+                  <span className="nav-link-label">Contact Us</span>
+                </Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Link aria-label="Go to About Us Page" className="navbar-link" to="/about us" onClick={() => setExpand(false)}>
+                  <span className="nav-link-label">About Us</span>
+                </Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Link aria-label="Go to Cart Page" className="navbar-link" to="/cart" onClick={() => setExpand(false)}>
+                  <span className="nav-link-label">Cart</span>
+                </Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
