a86fb0d49025b21f23d14fc9d2be6c906c073c7c
abdulrehmansafdar <abdulrehmansafdar2928@gmail.com>
2023-10-12 17:29:56 +0500

    manufacture section code updated

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,22 +1,22 @@
 import { Container, Row } from "react-bootstrap"
 import Product from "./Product/Product"
 
-const Section = ({ title, bgColor , productItems, addToCart }) => {
-return (
-    <section style={{ background:bgColor }}> 
-        <Container>
-            <div className='heading'>
-                <h1>{title}</h1>
-            </div>
-            <Row className="justify-content-center">
-                {productItems.map((productItem) => {
-                return (
-                    <Product key={productItem.id} title={title} productItem={productItem} addToCart={addToCart}/>
-                )
-                })}
-            </Row>
-        </Container>
-    </section>
+const Section = ({ title, bgColor, productItems, addToCart }) => {
+    return (
+        <section style={{ background: bgColor }}>
+            <Container>
+                <div className='heading'>
+                    <h1>{title}</h1>
+                </div>
+                <Row className="justify-content-center">
+                    {productItems.map((productItem) => {
+                        return (
+                            <Product key={productItem.id} title={title} productItem={productItem} addToCart={addToCart} />
+                        )
+                    })}
+                </Row>
+            </Container>
+        </section>
     )
 }
 
